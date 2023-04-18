60e4fb2fd6f692ea92e6a1c01c2a6fb9b245cb9b
JoaoPedroCardoso <joaopedrocar@hotmail.com>
2023-04-18 20:35:04 -0300

    update avaliacoes

--- components/posts/js/avaliacoes.js
+++ components/posts/js/avaliacoes.js
@@ -4,7 +4,7 @@ import img03Url from '/components/posts/img/foto03.jpg'
 
 const avaliacoes = [
     { foto: img01Url, titulo: "Ana Bruck", recomendacao: "“Eu recomendei a Letícia, pois o atendimento dela fez toda a diferença”", depoimento: "“A escolha de um imóvel, no nosso caso uma casa, não é uma tarefa Fácil. A escolha de um profissional qualificado poupou o nosso tempo e nos ajudou a encontrar o que realmente queriamos”" },
-    { foto: img02Url, titulo: "Dani Shutz", recomendacao: "“Eu recomendei a Letícia, pois o atendimento dela fez toda a diferença”", depoimento: "“A escolha de um imóvel, no nosso caso uma casa, não é uma tarefa Fácil. A escolha de um profissional qualificado poupou o nosso tempo e nos ajudou a encontrar o que realmente queriamos”" },
-    { foto: img03Url, titulo: "Bia Wolf", recomendacao: "“Eu recomendei a Letícia, pois o atendimento dela fez toda a diferença”", depoimento: "“A escolha de um imóvel, no nosso caso uma casa, não é uma tarefa Fácil. A escolha de um profissional qualificado poupou o nosso tempo e nos ajudou a encontrar o que realmente queriamos”" }
+    { foto: img02Url, titulo: "Dani Shutz", recomendacao: "“Eu recomendei a Letícia, pois ela é muito profissional”", depoimento: "“A escolha de um imóvel, no nosso caso um apartamento, não é uma tarefa Fácil. A escolha de um profissional qualificado poupou o nosso tempo e nos ajudou a encontrar o que realmente queriamos”" },
+    { foto: img03Url, titulo: "Bia Wolf", recomendacao: "“Eu recomendei a Letícia, pois tenho muita admiraçao ao trabalho dela”", depoimento: "“A escolha de um veiculo, no nosso caso um hatch economico, não é uma tarefa Fácil. A escolha de um profissional qualificado poupou o nosso tempo e nos ajudou a encontrar o que realmente queriamos”" }
 ]
 export default avaliacoes;
